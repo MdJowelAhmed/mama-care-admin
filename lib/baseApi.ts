49dba572b487a1186d1f2f67083bdc62e7e99178
Md Jowel Ahmed <155435567+MdJowelAhmed@users.noreply.github.com>
2025-09-10 17:11:59 +0600

    update some things

--- lib/baseApi.ts
+++ lib/baseApi.ts
@@ -1,6 +1,6 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-export const baseUrl = 'http://10.10.7.111:5001/api/v1';
+export const baseUrl = 'http://72.60.65.122:5001/api/v1';
 export const imageUrl = 'http://10.10.7.111:5001';
 
 // Centralized base query with automatic token authorization
